3053c228290bd20824b6ebbcf11d5164e465e2b1
S0016287648 <jerry.hsu@utegration.com>
2017-05-22 19:48:16 +0000

    initial upload

---
+++ webapp/controller/Main.controller.js
@@ -0,0 +1,9 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function(Controller) {
+	"use strict";
+
+	return Controller.extend("com.jhsu.tutorialsapui5-tutorial-walkthrough.controller.Main", {
+
+	});
+});
